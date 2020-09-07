a22db9806f77d6820b06db990c5e12ef7c863b8c
ianardavis <48180829+ianardavis@users.noreply.github.com>
2020-09-07 14:56:00 +0100

    Update newUserFieldCheck.js

--- public/js/newUserFieldCheck.js
+++ public/js/newUserFieldCheck.js
@@ -1,37 +1,22 @@
-var input_bader     = document.querySelector("#_bader"),
-    input_name      = document.querySelector("#_name"),
-    input_ini       = document.querySelector("#_ini"),
-    input_login_id  = document.querySelector("#_login_id"),
-    input_password  = document.querySelector("#_password"),
-    input_confirm   = document.querySelector("#_confirm"),
-    btn_submit      = document.querySelector("#_save");
-
-checkFilled = () => {
-    if (bad_password() || 
-        input_bader.value    === '' || 
-        input_name.value     === '' || 
-        input_ini.value      === '' || 
-        input_login_id.value === '' || 
-        input_password.value === '' || 
-        input_confirm.value  === '') {
-        btn_submit.disabled= true;
-    } else btn_submit.disabled= false;
+var _bader    = document.querySelector("#_bader"),
+    _name     = document.querySelector("#_name"),
+    _ini      = document.querySelector("#_ini"),
+    _login_id = document.querySelector("#_login_id"),
+    _password = document.querySelector("#_password"),
+    _confirm  = document.querySelector("#_confirm"),
+    _save     = document.querySelector("#_save");
+check_fields = () => {
+    if (_bader.value    !== '' && 
+        _name.value     !== '' && 
+        _ini.value      !== '' && 
+        _login_id.value !== '') {
+        if (password_ok) _save.disabled = false;
+        else _save.disabled = true;
+    } else _save.disabled = true;
 };
-bad_password = () => {
-    if (input_password.value !== input_confirm.value ||
-        input_password.value.length < 8) {
-            input_password.classList.add('pwd_warn');
-            input_confirm.classList.add('pwd_warn');
-            return true;
-    } else {
-        input_password.classList.remove('pwd_warn');
-        input_confirm.classList.remove('pwd_warn');
-        return false;
-    };
-};
-input_password.addEventListener('input', checkFilled);
-input_confirm.addEventListener('input',  checkFilled);
-input_bader.addEventListener('input',    checkFilled);
-input_name.addEventListener('input',     checkFilled);
-input_ini.addEventListener('input',      checkFilled);
-input_login_id.addEventListener('input', checkFilled);
+_password.addEventListener('input', check_fields);
+_confirm.addEventListener('input',  check_fields);
+_bader.addEventListener('input',    check_fields);
+_name.addEventListener('input',     check_fields);
+_ini.addEventListener('input',      check_fields);
+_login_id.addEventListener('input', check_fields);
