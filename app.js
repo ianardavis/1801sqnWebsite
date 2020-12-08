05e21fabe4977afc1af974f1edb7aaa2e5ff2274
Ian Davis <ianar.davis@gmail.com>
2020-12-08 13:38:06 +0000

    test commit vs code

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
 console.log(`Environment: ${process.env.NODE_ENV}`);
 console.log('   Set')
+console.log(__dirname);
 let port_check = require(`${process.env.ROOT}/fn/utils/port_check`)
 port_check()
 .then(port => {
