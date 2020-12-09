30cdd6787f93943e5db6370e973aa038627d87d0
Ian Davis <ianar.davis@gmail.com>
2020-12-09 16:55:12 +0000

    fix common notifications

--- routes/common/notifications.js
+++ routes/common/notifications.js
@@ -1,8 +1,6 @@
 module.exports = (app, allowed, permissions, m, db) => {
     app.get(`/${db}/get/notifications`, permissions, allowed(`access_${db}`), (req, res) => {
-        m[db].notifications.findAll({
-            where: {user_id: req.user.user_id}
-        })
+        m.notifications.findAll({where: {user_id: req.user.user_id}})
         .then(notifications => res.send({result: true, notifications: notifications}))
         .catch(err => res.error.send(err, res));
     });
