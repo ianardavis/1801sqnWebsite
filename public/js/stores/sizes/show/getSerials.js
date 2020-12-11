e981dc6683973ee59b07251b287bc1999385930a
Ian Davis <ianar.davis@gmail.com>
2020-12-11 14:06:13 +0000

    add serial

--- public/js/stores/sizes/show/getSerials.js
+++ public/js/stores/sizes/show/getSerials.js
@@ -1,21 +1,21 @@
 function getSerials() {
     get(
         function (serials, options) {
-            try {
                 clearElement('tbl_serials');
                 let table_body = document.querySelector('#tbl_serials');
                 set_count({id: 'serial', count: serials.length});
-                serials.forEach(serial => {
+            serials.forEach(serial => {
+                try {
                     let row = table_body.insertRow(-1);
                     add_cell(row, {text: serial._serial});
                     add_cell(row, {text: serial.location._location});
                     add_cell(row, {append: new Link({
                         href: `javascript:show("serials",${serial.serial_id})`,
                         small: true}).e});
-                });
-            } catch (error) {
-                console.log(error);
-            };
+                } catch (error) {
+                    console.log(error);
+                };
+            });
         },
         {
             table: 'serials',
