36bc3230cf7f40a3dc1807e8dfb3ba0585c7e26d
Ian Davis <ianar.davis@gmail.com>
2021-05-17 19:22:19 +0100

    loancard initial

---
+++ public/js/stores/loancards/index/view.js
@@ -0,0 +1,43 @@
+let loancard_statuses   = {"0": "Cancelled", "1": "Draft", "2": "Complete", "3":"Closed"};
+function getLoancards() {
+    clear_table('loancards')
+    .then(tbl_loancards => {
+        let sel_status = document.querySelector('#sel_status') || {value: ''},
+            sel_users  = document.querySelector('#sel_users')  || {value: ''};
+        get(
+            {
+                table: 'loancards',
+                query: [sel_status.value, sel_users.value]
+            },
+            
+        )
+        .then(function ([loancards, options]) {
+            loancards.forEach(loancard => {
+                let row = tbl_loancards.insertRow(-1);
+                add_cell(row, table_date(loancard.createdAt));
+                add_cell(row, {text: print_user(loancard.user_loancard)});
+                add_cell(row, {text: loancard.lines.length});
+                add_cell(row, {text: loancard_statuses[loancard.status]});
+                add_cell(row, {append: new Link({href: `/loancards/${loancard.loancard_id}`, small: true}).e});
+            });
+        });
+    });
+};
+function loadGetLoancards() {
+    let get_interval = window.setInterval(
+        function () {
+            if (users_loaded === true) {
+                getLoancards();
+                clearInterval(get_interval);
+            }
+        },
+        200
+    );
+};
+addReloadListener(loadGetLoancards);
+window.addEventListener('load', function () {
+    addListener('reload_users', function () {listUsers({blank: true, blank_opt: {text: 'All'}})});
+    addListener('sel_status', loadGetLoancards)
+    document.querySelector('#sel_status').addEventListener('change', loadGetLoancards);
+    document.querySelector('#sel_users') .addEventListener('change', loadGetLoancards);
+});
