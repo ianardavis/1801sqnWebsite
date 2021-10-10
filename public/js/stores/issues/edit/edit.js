94db77e4764b4c2e41454cf11a1a17a460f2a4a9
Ian Davis <ianar.davis@gmail.com>
2021-10-10 21:47:54 +0100

    update 1

--- public/js/stores/issues/edit/edit.js
+++ public/js/stores/issues/edit/edit.js
@@ -41,6 +41,7 @@ function addEditSelect() {
                     };
                 })
                 .then(issue => {
+                    console.log(issue);
                     if (issue.status === 1            && issue.size.issueable && typeof addApproveOption  === 'function') addApproveOption( issue.issue_id);
                     if (issue.status === 2            && issue.size.orderable && typeof addOrderOption    === 'function') addOrderOption(   issue.issue_id);
                     if ([2, 3].includes(issue.status) && issue.size.issueable && typeof addIssueOption    === 'function') addIssueOption(   issue.issue_id);
