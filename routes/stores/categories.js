fd17a910b8102ef09b1ecae4a1bf7f81335ba82f
Ian Davis <ianar.davis@gmail.com>
2022-04-04 21:00:52 +0100

    category update2

--- routes/stores/categories.js
+++ routes/stores/categories.js
@@ -1,8 +1,8 @@
 module.exports = (app, m, fn) => {
     app.get('/get/categories',    fn.loggedIn(), fn.permissions.check('access_stores'), (req, res) => {
         // let query = fn.nullify(req.query.where);
-        console.log(req.query.where);
         if (req.query.where.category_id_parent && req.query.where.category_id_parent === "") req.query.where.category_id_parent = null;
+        console.log(req.query.where);
         m.categories.findAndCountAll({
             where:   req.query.where || {},
             include: [fn.inc.stores.categories({as: 'parent'})],
