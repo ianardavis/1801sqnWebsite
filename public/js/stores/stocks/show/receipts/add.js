df3901f244c2842a281129e4185a953c73a346ca
Ian Davis <ianar.davis@gmail.com>
2022-07-11 20:54:25 +0100

    add receipt on stock page

---
+++ public/js/stores/stocks/show/receipts/add.js
@@ -0,0 +1,18 @@
+function reset_receipt_add() {
+    set_value('receipt_qty');
+};
+window.addEventListener('load', function () {
+    modalOnShow('receipt_add', reset_receipt_add);
+    enable_button('receipt_add');
+    addFormListener(
+        'receipt_add',
+        'POST',
+        '/stocks/receipts',
+        {
+            onComplete: [
+                getStock,
+                function () {modalHide('receipt_add')}
+            ]
+        }
+    );
+});
