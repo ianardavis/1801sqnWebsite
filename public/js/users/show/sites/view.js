dfdbf855eb0b6200615bfd5cff6aa40acfd43429
Ian Davis <ianar.davis@gmail.com>
2024-01-19 22:43:28 +0000

    update

---
+++ public/js/users/show/sites/view.js
@@ -0,0 +1,21 @@
+function getUserSites() {
+    clear('tbl_sites')
+    .then(tbl_sites => {
+        get({
+            location: `sites/user/${path[2]}`,
+            table: 'sites',
+            func: getUserSites
+        })
+        .then(function ([results, options]) {
+            setCount('site', results.length);
+            results.forEach(site => {
+                let row = tbl_sites.insertRow(-1);
+                addCell(row, {text: site.name});
+                addCell(row, {text: (site.user_sites.is_default ? "Yes" : "")});
+            });
+        });
+    });
+};
+window.addEventListener("load", function () {
+    getUserSites();
+});
